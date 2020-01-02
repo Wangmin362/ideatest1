cff750ad62e681718ae26a6e41f2aab173444f32
david <1614586246@qq.com>
2020-01-02 16:49:34 +0800

    创建初始项目

---
+++ src/main/java/com/davod/ideatest1/Ideatest1Application.java
@@ -0,0 +1,13 @@
+package com.davod.ideatest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ideatest1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ideatest1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/davod/ideatest1/Ideatest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.davod.ideatest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ideatest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
